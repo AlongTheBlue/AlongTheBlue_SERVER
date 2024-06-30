8f96793d020b7659b063de81fe8edf7794e559dc
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-06-30 23:53:18 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AlongTheBlue_SERVER
+ 바당따라 백엔드
---
+++ src/test/java/org/alongtheblue/alongtheblue_server/AlongTheBlueServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.alongtheblue.alongtheblue_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlongTheBlueServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
