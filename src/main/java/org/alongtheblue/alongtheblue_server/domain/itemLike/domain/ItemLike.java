2799e90304a21db01969730302f5c7581653da6e
MoonInbae <mib010213@gmail.com>
2024-09-05 13:43:13 +0900

    [Feat] TourCourse 관계 추가

--- src/main/java/org/alongtheblue/alongtheblue_server/domain/itemLike/domain/ItemLike.java
+++ src/main/java/org/alongtheblue/alongtheblue_server/domain/itemLike/domain/ItemLike.java
@@ -3,31 +3,40 @@ package org.alongtheblue.alongtheblue_server.domain.itemLike.domain;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.alongtheblue.alongtheblue_server.domain.hashTag.domain.HashTag;
+import org.alongtheblue.alongtheblue_server.domain.hashTag_item.domain.HashTag_item;
+import org.alongtheblue.alongtheblue_server.domain.item.domain.Item;
 import org.alongtheblue.alongtheblue_server.domain.userInfo.domain.UserInfo;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ItemLike {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @NotNull
-    private Date date;
+    @EmbeddedId
+    private ItemLike.ItemLikeId id;
 
     @ManyToOne
-    @JoinColumn(name = "userId", nullable = false)
+    @MapsId("itemId")
+    @JoinColumn(name = "itemId", nullable = false)
     @JsonBackReference
-    private UserInfo userInfo;
-    @Builder
-    public ItemLike(Date date) {
-        this.date = date;
+    private UserInfo user;
+
+    @ManyToOne
+    @MapsId("hid")
+    @JoinColumn(name = "hid", nullable = false)
+    @JsonBackReference
+    private HashTag hashTag;
+
+    @Embeddable
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class ItemLikeId implements Serializable {
+        private Long itemId;
+        private Long hid;
     }
 }
--- src/main/java/org/alongtheblue/alongtheblue_server/domain/tourCourse_item/domain/TourCourse_Item.java
+++ src/main/java/org/alongtheblue/alongtheblue_server/domain/tourCourse_item/domain/TourCourse_Item.java
@@ -3,33 +3,43 @@ package org.alongtheblue.alongtheblue_server.domain.tourCourse_item.domain;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.alongtheblue.alongtheblue_server.domain.hashTag.domain.HashTag;
+import org.alongtheblue.alongtheblue_server.domain.hashTag_item.domain.HashTag_item;
+import org.alongtheblue.alongtheblue_server.domain.item.domain.Item;
 import org.alongtheblue.alongtheblue_server.domain.tourCourse.domain.TourCourse;
 import org.alongtheblue.alongtheblue_server.domain.userInfo.domain.UserInfo;
 
+import java.io.Serializable;
 import java.util.Date;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class TourCourse_Item {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @EmbeddedId
+    private TourCourse_Item.TourCourseItemId id;
 
-    @NotNull
     private Date date;
 
     @ManyToOne
-    @JoinColumn(name = "courseId", nullable = false)
+    @MapsId("itemId")
+    @JoinColumn(name = "itemId", nullable = false)
+    @JsonBackReference
+    private Item item;
+
+    @ManyToOne
+    @MapsId("tid")
+    @JoinColumn(name = "tid", nullable = false)
     @JsonBackReference
     private TourCourse tourCourse;
 
-    @Builder
-    public TourCourse_Item(Date date) {
-        this.date = date;
+    @Embeddable
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public class TourCourseItemId implements Serializable {
+        private Long itemId;
+        private Long tid;
     }
 }
