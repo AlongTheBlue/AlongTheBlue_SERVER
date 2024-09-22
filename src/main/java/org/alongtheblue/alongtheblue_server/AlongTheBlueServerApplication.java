3ed432b0c3ea63b11933580445ba4b95af09f993
yeilkk <yeil99@naver.com>
2024-09-22 15:38:01 +0900

    feat: toruData DB 추가

--- src/main/java/org/alongtheblue/alongtheblue_server/AlongTheBlueServerApplication.java
+++ src/main/java/org/alongtheblue/alongtheblue_server/AlongTheBlueServerApplication.java
@@ -1,7 +1,11 @@
 package org.alongtheblue.alongtheblue_server;
 
+import org.alongtheblue.alongtheblue_server.global.data.tourData.TourData;
+import org.alongtheblue.alongtheblue_server.global.data.tourData.TourDataService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class AlongTheBlueServerApplication {
