4ec5830954cac5b4eb74e69383ea5e9d6dbeb7cf
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-09-30 16:17:44 +0900

    [Feat] 스케줄러 허용

--- src/main/java/org/alongtheblue/alongtheblue_server/AlongTheBlueServerApplication.java
+++ src/main/java/org/alongtheblue/alongtheblue_server/AlongTheBlueServerApplication.java
@@ -5,11 +5,13 @@ import org.alongtheblue.alongtheblue_server.global.data.tourData.TourDataService
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.ArrayList;
 
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@EnableScheduling
 public class AlongTheBlueServerApplication {
 
 	public static void main(String[] args) {
