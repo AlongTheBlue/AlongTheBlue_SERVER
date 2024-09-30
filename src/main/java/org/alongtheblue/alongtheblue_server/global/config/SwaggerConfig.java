2821e538db4a1abc3b614a9d1864cd64198c5647
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-09-30 13:46:37 +0900

    [Feat] Swagger 설정
    - 배포환경(https)와 로컬환경(http://localhost:8080)에서 모두 동작하도록 설정

--- src/main/java/org/alongtheblue/alongtheblue_server/global/config/SwaggerConfig.java
+++ src/main/java/org/alongtheblue/alongtheblue_server/global/config/SwaggerConfig.java
@@ -1,20 +1,26 @@
 package org.alongtheblue.alongtheblue_server.global.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-    info = @Info(title = "바당따라",
-            description = "바당따라 Api 명세서",
-            version = "v1"))
+import java.util.List;
+
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
+
+    // application.yml에서 설정한 서버 URL을 읽어옴
+    @Value("${openapi.servers.url}")
+    private String serverUrl;
+
     @Bean
     public GroupedOpenApi chatOpenApi() {
         String[] paths = {"/api/**"};
@@ -25,8 +31,20 @@ public class SwaggerConfig {
                 .name("session_id");
 
         return GroupedOpenApi.builder()
-                .group("Studio_I Promotion Page API v1")
+                .group("바당따라 Page API v1")
                 .pathsToMatch(paths)
                 .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("바당따라")
+                        .description("바당따라 Api 명세서")
+                        .version("v1"))
+                .servers(List.of(new Server()
+                        .url(serverUrl)
+                        .description("Configured Server")));
     }
 }
