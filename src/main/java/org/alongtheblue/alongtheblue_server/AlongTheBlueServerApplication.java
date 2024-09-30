c6c073d98136baf253c0877aeacd847739ee4ead
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-09-30 16:14:49 +0900

    [Feat] Spring Security 설정

--- src/main/java/org/alongtheblue/alongtheblue_server/AlongTheBlueServerApplication.java
+++ src/main/java/org/alongtheblue/alongtheblue_server/AlongTheBlueServerApplication.java
@@ -4,10 +4,12 @@ import org.alongtheblue.alongtheblue_server.global.data.tourData.TourData;
 import org.alongtheblue.alongtheblue_server.global.data.tourData.TourDataService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 import java.util.ArrayList;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class AlongTheBlueServerApplication {
 
 	public static void main(String[] args) {
