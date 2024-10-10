993a04fd05b65ab8ee50885b2f59b46e16ffc2a8
yeilkk <yeil99@naver.com>
2024-10-11 01:41:03 +0900

    feat: 바당따라 코스 조회

---
+++ src/main/java/org/alongtheblue/alongtheblue_server/global/data/alongBlues/dto/response/BlueItemResponseDto.java
@@ -0,0 +1,11 @@
+package org.alongtheblue.alongtheblue_server.global.data.alongBlues.dto.response;
+
+public record BlueItemResponseDto(
+        String title,
+        String address,
+        String x,
+        String y,
+        String category
+) {
+
+}
