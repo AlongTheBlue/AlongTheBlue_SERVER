f1c8e099d3a725e375d0fd7fcb51773e76e2bde4
MoonInbae <mib010213@gmail.com>
2024-10-13 05:10:31 +0900

    [Feat] UserTourCourse UserInfo 관계 생성

--- src/main/java/org/alongtheblue/alongtheblue_server/global/data/tourcommunity/UserTourCourse.java
+++ src/main/java/org/alongtheblue/alongtheblue_server/global/data/tourcommunity/UserTourCourse.java
@@ -1,8 +1,10 @@
 package org.alongtheblue.alongtheblue_server.global.data.tourcommunity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.alongtheblue.alongtheblue_server.domain.userInfo.domain.UserInfo;
 
 import java.util.Date;
 import java.util.List;
@@ -22,6 +24,11 @@ public class UserTourCourse {
     @OneToMany(mappedBy = "userTourCourse", cascade = CascadeType.ALL)
     @JsonManagedReference
     private List<TourPostItem> tourPostItems;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    @JsonBackReference
+    private UserInfo userInfo;
 
 //    @OneToMany(mappedBy = "tourCourseForHashTag", cascade = CascadeType.ALL)
 //    @JsonManagedReference
