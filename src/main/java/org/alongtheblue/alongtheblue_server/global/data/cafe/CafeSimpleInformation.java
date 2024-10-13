adebc6394a236fa62e35407e762cf46467caced1
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-13 23:45:20 +0900

    [Refactor] 불필요한 import 제거

--- src/main/java/org/alongtheblue/alongtheblue_server/global/data/cafe/CafeSimpleInformation.java
+++ src/main/java/org/alongtheblue/alongtheblue_server/global/data/cafe/CafeSimpleInformation.java
@@ -1,7 +1,5 @@
 package org.alongtheblue.alongtheblue_server.global.data.cafe;
 
-import org.alongtheblue.alongtheblue_server.global.data.restaurant.RestaurantImage;
-
 import java.util.List;
 
 public interface CafeSimpleInformation {
