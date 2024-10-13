56cea3eec9953485405d52e05151051a1f5562bc
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-14 06:17:04 +0900

    [Feat] cafe 검색 pagination 구현

--- src/main/java/org/alongtheblue/alongtheblue_server/global/data/cafe/CafeRepository.java
+++ src/main/java/org/alongtheblue/alongtheblue_server/global/data/cafe/CafeRepository.java
@@ -1,12 +1,12 @@
 package org.alongtheblue.alongtheblue_server.global.data.cafe;
 
 import org.alongtheblue.alongtheblue_server.global.data.global.SimpleInformation;
+import org.alongtheblue.alongtheblue_server.global.data.search.SearchInformation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
@@ -14,7 +14,8 @@ public interface CafeRepository extends JpaRepository<Cafe, Long> {
     @Query("SELECT c FROM Cafe c JOIN c.images i GROUP BY c HAVING COUNT(i) > 0")
     Page<SimpleInformation> findAllSimple(Pageable pageable);
 
-    List<Cafe> findByTitleContaining(String keyword);
+    @Query("SELECT c FROM Cafe c JOIN c.images i WHERE c.title LIKE %:keyword% GROUP BY c HAVING COUNT(i) > 0")
+    Page<SearchInformation> findByTitleContaining(String keyword, Pageable pageable);
 
     Optional<Cafe> findByContentId(String contentId);
 }
