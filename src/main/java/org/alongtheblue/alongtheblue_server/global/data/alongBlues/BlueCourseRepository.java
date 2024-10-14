4045979753eccfcb058b9f54fe0098ee77255a31
MoonInbae <81071736+MoonInbae@users.noreply.github.com>
2024-10-14 16:21:51 +0900

    [Feat] 내 바당따라 전체 코스 조회 API 구현

--- src/main/java/org/alongtheblue/alongtheblue_server/global/data/alongBlues/BlueCourseRepository.java
+++ src/main/java/org/alongtheblue/alongtheblue_server/global/data/alongBlues/BlueCourseRepository.java
@@ -2,6 +2,10 @@ package org.alongtheblue.alongtheblue_server.global.data.alongBlues;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface BlueCourseRepository extends JpaRepository<BlueCourse, Long> {
+
+    List<BlueCourse> findByUserInfo_Uid(String uid);
 }
